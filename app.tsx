1f05f35b83323cee74b0e792b56eda1fc9c092bf
samuel21695 <samuel21695@gmail.com>
2024-01-03 22:33:11 +0900

    ✨ function counter

--- app.tsx
+++ app.tsx
@@ -16,3 +16,21 @@ interface Counter {
  * 제작자는 Counter 인터페이스가 무엇인지 몰라도,
  * 작성하면서 에러를 통해 인터페이스를 따르지 않았다는 것을 알 수 있다.
  */
+
+
+function counter() : Counter {
+  let count:number = 0; //private 변수, 외부에서 접근 불가능
+  // 함수형 프로그래밍에서는 외부에서 접근 불가능한 변수를 '클로저'라고 한다.
+  // 보안측면에서 count 변수는 제작자'만' 접근이 가능하다.
+  return {
+    increment() :void {
+      count++;
+    },
+    decrement() :void {
+      count--;
+    },
+    getCount() :number {
+      return count;
+    }
+  }
+}
